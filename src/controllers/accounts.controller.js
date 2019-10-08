a79b59768f976c866d18a529a708f3136f125138
vansi99 <vansido99@gmail.com>
2019-10-08 17:19:48 +0700

    initialize ci

--- src/controllers/accounts.controller.js
+++ src/controllers/accounts.controller.js
@@ -4,9 +4,10 @@ const responseUtil = require("../utils/response.util");
 async function getAccounts(req, res) {
     try {
         const [rows,fields] = await dbPool.query("select * from accounts");
+        throw new Error("test");
         res.send(responseUtil.success({data: rows}));
     } catch (err) {
-        res.send(err.message)
+        res.send(responseUtil.fail({reason: err.message}))
     }
 }
 

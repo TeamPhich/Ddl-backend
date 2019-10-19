69fa894d1402ca285863e152efced6913b28c0a7
HongHanh120 <lehonghanh.120@gmail.com>
2019-10-20 00:13:21 +0700

    create space

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const config = require("config");
 
 const accountsRoute = require("./routes/accounts.route");
+const spacesRoute = require("./routes/spaces.route");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('combined'));
 
 app.use("/api/v1/accounts/", accountsRoute);
+app.use("/api/v1/spaces/", spacesRoute);
 
 app.use("/api/v1", (req, res) => {
     res.send("ddl-backend");

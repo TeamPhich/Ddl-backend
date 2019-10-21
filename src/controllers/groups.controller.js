fbe4533b84b0040ea3909be384c5bb016283567e
nguyen-thuong <nguyentrongthuong1998@gmail.com>
2019-10-21 13:51:12 +0700

    config

--- src/controllers/groups.controller.js
+++ src/controllers/groups.controller.js
@@ -10,7 +10,36 @@ async function createGroup(req, res) {
         const [existGroups] = await dbPool.query(`SELECT * FROM spaces INNER JOIN groups ON spaces.id = groups.space_id AND groups.name = "${name}"`);
         if (existGroups.length) throw new Error("group_name existed");
         const temp = await  dbPool.query(`INSERT INTO groups (space_id,name) VALUES (${space_id},"${name}")`)
-        console.log(temp);
+        res.json(responseUtil.success({data: {}}))
+    } catch (err) {
+        res.json(responseUtil.fail({reason: err.message}))
+    }
+}
+
+async function addMembers(req, res) {
+    const {
+        user_ids,
+        group_id
+    } = req.body
+    try{
+        user_ids.forEach(async function (user_id){
+            const temp = await dbPool.query(`INSERT INTO groups_members (user_id,group_id) VALUES ("${user_id}","${group_id}")`);
+        });
+        res.json(responseUtil.success({data: {}}))
+    } catch (err) {
+        res.json(responseUtil.fail({reason: err.message}))
+    }
+}
+
+async  function removeMembers(req, res) {
+    const {
+        user_ids,
+        group_id
+    } = req.body
+    try{
+        user_ids.forEach(async function (user_id){
+            const temp = await dbPool.query(`DELETE FROM groups_members WHERE user_id = "${user_id}" AND group_id = "${group_id}"`);
+        });
         res.json(responseUtil.success({data: {}}))
     } catch (err) {
         res.json(responseUtil.fail({reason: err.message}))
@@ -18,5 +47,7 @@ async function createGroup(req, res) {
 }
 
 module.exports = {
-    createGroup
+    createGroup,
+    addMembers,
+    removeMembers,
 };
--- src/routes/groups.route.js
+++ src/routes/groups.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const groupsController = require("../controllers/groups.controller")
 const token = require("../middleware/token");
 
-router.post("/create",token.verify ,groupsController.createGroup);
+router.post("/group",token.verify ,groupsController.createGroup);
+router.post("/members" ,token.verify ,groupsController.addMembers);
+router.put("/members" ,token.verify ,groupsController.removeMembers);
 
 module.exports = router;

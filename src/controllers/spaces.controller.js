a0c5fe1f09c5fb76879453e50f898baa27d0d69a
HongHanh120 <lehonghanh.120@gmail.com>
2019-10-31 09:49:36 +0700

    oke

--- src/controllers/spaces.controller.js
+++ src/controllers/spaces.controller.js
@@ -51,8 +51,11 @@ async function addMember(req, res) {
                             VALUES (${user_id}, ${space_id}, 2)`);
         let [group_id] = await dbPool.query(`select id from groups where name = "everyone" and space_id = ${space_id}`);
         group_id = group_id[0].id;
-        await dbPool.query(`INSERT INTO groups_members(user_id, group_id)
+        console.log(group_id);
+        await dbPool.query(`SET FOREIGN_KEY_CHECKS=0`);
+        await dbPool.query(`INSERT INTO groups_members(member_id, group_id)
                             VALUES (${user_id}, ${group_id})`);
+        await dbPool.query(`SET FOREIGN_KEY_CHECKS=1`);
         res.json(responseUtil.success({data: {}}));
     } catch (err) {
         res.send(responseUtil.fail({reason: err.message}))

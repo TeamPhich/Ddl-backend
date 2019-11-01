84d048f29dd8b827a1b8348f32fa2d35b1d66c08
HongHanh120 <lehonghanh.120@gmail.com>
2019-11-01 12:47:06 +0700

    oke

--- src/controllers/spaces.controller.js
+++ src/controllers/spaces.controller.js
@@ -6,21 +6,30 @@ async function createSpace(req, res) {
         const id = req.tokenData.id;
         const name = req.body.name;
         if (!name) throw new Error("name field is missing");
-        const [rows, fields] = await dbPool.query(`select * from spaces where name = "${name}"`);
+        const [rows, fields] = await dbPool.query(`select * 
+                                                   from spaces 
+                                                   where name = "${name}"`);
         if (rows.length) throw new Error("name was existed");
         await dbPool.query(`INSERT INTO spaces(user_id, name)
                             VALUES(${id},"${name}")`);
-        let [space_id] = await dbPool.query(`select id from spaces where name = "${name}"`);
+        let [space_id] = await dbPool.query(`select id 
+                                             from spaces 
+                                             where name = "${name}"`);
         space_id = space_id[0].id;
         await dbPool.query(`INSERT INTO spaces_members(user_id, space_id, role_id)
                             VALUES(${id}, ${space_id}, 1)`);
         await dbPool.query(`INSERT INTO groups(space_id, name, couple)
                             VALUES(${space_id}, "everyone", 0)`);
-        let [group_id] = await dbPool.query(`select id from groups where name = "everyone" and space_id = ${space_id}`);
+        let [group_id] = await dbPool.query(`select id 
+                                             from groups 
+                                             where name = "everyone" and space_id = ${space_id}`);
         group_id = group_id[0].id;
-        console.log(group_id);
-        await dbPool.query(`INSERT INTO groups_members(user_id, group_id)
-                            VALUES(${id}, ${group_id})`);
+        let [member_id] = await dbPool.query(`select id
+                                              from spaces_members
+                                              where user_id = ${id} and space_id = ${space_id}`);
+        member_id = member_id[0].id;
+        await dbPool.query(`INSERT INTO groups_members(member_id, group_id)
+                            VALUES(${member_id}, ${group_id})`);
         res.json(responseUtil.success({data: {}}));
     } catch (err) {
         res.send(responseUtil.fail({reason: err.message}))
@@ -30,7 +39,10 @@ async function createSpace(req, res) {
 async function getListSpace(req, res) {
     try {
         const id = req.tokenData.id;
-        const [rows] = await dbPool.query(`select name, id from spaces where user_id = ${id}`);
+        const [rows] = await dbPool.query(`select spaces.name, spaces_members.id 
+                                           from spaces_members
+                                           inner join spaces on spaces_members.space_id = spaces.id
+                                           where spaces_members.user_id = ${id}`);
         res.json(responseUtil.success({data: {rows}}));
     } catch (err) {
         res.send(responseUtil.fail({reason: err.message}))
@@ -40,18 +52,31 @@ async function getListSpace(req, res) {
 async function addMember(req, res) {
     const id = req.tokenData.id;
     const {user_id, space_id} = req.body;
-    const [user, fields] = await dbPool.query(`select user_id from spaces_members where (user_id = ${id} and space_id = ${space_id})`);
     try {
+        if (!space_id)
+            throw new Error("missing space_id!");
+        const [user] = await dbPool.query(`select user_id 
+                                            from spaces_members 
+                                            where user_id = ${id} and space_id = ${space_id}`);
+        if (!user_id)
+            throw new Error("missing user_id!");
+
         if (!user.length)
             throw new Error("user is not in space");
-        const [member, fields] = await dbPool.query(`select user_id from spaces_members where user_id = ${user_id} and space_id = ${space_id}`);
+        const [member] = await dbPool.query(`select user_id 
+                                            from spaces_members 
+                                            where user_id = ${user_id} and space_id = ${space_id}`);
         if (member.length)
             throw new Error("user was in space");
         await dbPool.query(`INSERT INTO spaces_members(user_id, space_id, role_id) 
                             VALUES (${user_id}, ${space_id}, 2)`);
-        let [group_id] = await dbPool.query(`select id from groups where name = "everyone" and space_id = ${space_id}`);
+        let [group_id] = await dbPool.query(`select id 
+                                             from groups 
+                                             where name = "everyone" and space_id = ${space_id}`);
         group_id = group_id[0].id;
-        let [member_id] = await dbPool.query(`select id from spaces_members where user_id = ${user_id} and space_id = ${space_id}`);
+        let [member_id] = await dbPool.query(`select id 
+                                             from spaces_members 
+                                             where user_id = ${user_id} and space_id = ${space_id}`);
         member_id = member_id[0].id;
         await dbPool.query(`INSERT INTO groups_members(member_id, group_id)
                             VALUES (${member_id}, ${group_id})`);
@@ -64,11 +89,19 @@ async function addMember(req, res) {
 async function getListMember(req, res) {
     const id = req.tokenData.id;
     const {space_id} = req.body;
-    const [user, fields] = await dbPool.query(`select user_id from spaces_members where user_id = ${id} and space_id = ${space_id}`);
+
     try {
+        if (!space_id)
+            throw new Error("missing space_id!");
+        const [user] = await dbPool.query(`select user_id 
+                                               from spaces_members 
+                                               where user_id = ${id} and space_id = ${space_id}`);
         if (!user.length)
             throw new Error("user is not in space");
-        const [rows, fields] = await dbPool.query(`select user_id, accounts.user_name from spaces_members inner join accounts on user_id = accounts.id where space_id = ${space_id}`);
+        const [rows] = await dbPool.query(`select user_id, accounts.user_name, accounts.full_name 
+                                                   from spaces_members 
+                                                   inner join accounts on user_id = accounts.id 
+                                                   where space_id = ${space_id}`);
         res.json(responseUtil.success({data: {rows}}));
     } catch (err) {
         res.send(responseUtil.fail({reason: err.message}))

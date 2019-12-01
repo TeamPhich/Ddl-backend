4e43cce0d2ef7975cc5dcd6505385933aece6635
nguyen-thuong <nguyentrongthuong1998@gmail.com>
2019-12-01 14:44:27 +0700

    Merge branch 'dev' of https://github.com/TeamPhich/Ddl-backend into thuong
    # Conflicts:
    #	src/controllers/jobs.controller.js

--- src/routes/groups.route.js
+++ src/routes/groups.route.js
@@ -8,6 +8,7 @@ router.post("/", spaceToken.verify, groupsController.createGroup);
 router.get("/members/:group_id", spaceToken.verify, groupsController.getMembers);
 router.post("/members", spaceToken.verify, groupsController.addMembers);
 router.put("/members", spaceToken.verify, groupsController.removeMembers);
+router.get("/members/not_in/:group_id", spaceToken.verify, groupsController.getMembersNotInGroup);
 
 
 module.exports = router;

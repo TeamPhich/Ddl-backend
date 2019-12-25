dafff144b214fa023accdca17d496072b182794b
vansi99 <vansido99@gmail.com>
2019-12-25 14:13:25 +0700

    ok

--- src/routes/spaces.route.js
+++ src/routes/spaces.route.js
@@ -13,7 +13,7 @@ router.post("/members", tokenCurrentSpace.verify, spacesController.addMember);
 router.get("/members", tokenCurrentSpace.verify, spacesController.getMemberList);
 router.delete("/members", tokenCurrentSpace.verify, member.spaceVerify, privilege.verify(2), roleLayer.verify, spacesController.removeMember);
 router.delete("/leavings", tokenCurrentSpace.verify, privilege.verify(5), spacesController.leaveSpace);
-router.put("/roles", tokenCurrentSpace.verify, member.spaceVerify, privilege.verify(3), roleLayer.verify ,spacesController.changeRoles);
+router.put("/roles", tokenCurrentSpace.verify, member.spaceVerify, roleLayer.verify ,spacesController.changeRoles);
 router.delete("/", tokenCurrentSpace.verify, privilege.verify(8), spacesController.deleteSpace);
 router.get("/profiles", tokenCurrentSpace.verify, spacesController.getProfile);
 router.put("/profiles", tokenCurrentSpace.verify, spacesController.putProfile);
